280dc20c60ac9de7b62286a579a71c86553341ba
Jamilly <jamillysc@hotmail.com>
2022-05-29 21:22:12 -0300

    pascalcode

---
+++ codep/MaiormenorIdade Linguagem Pascal..pas
@@ -0,0 +1,15 @@
+Program MaiormenorIdade ;
+Var
+ano: integer;
+Begin
+
+   writeln('Digite o ano que você nasceu');
+   read(ano);
+   
+   if(ano<=2004)then
+   writeln('Você é maior de idade')
+   
+    else
+    writeln('Você é menor de idade');
+   
+End.
